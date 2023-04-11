4fd9bc1ca98e2b823bdf9060eaf48a019544c7a5
MatheusPires12 <126611258+MatheusPires12@users.noreply.github.com>
2023-04-11 15:51:03 -0300

    Add files via upload

---
+++ jogodaforca/js/forca.js
@@ -0,0 +1,385 @@
+let jogarNovamente = true;
+let tentativas = 6;
+let listaDinamica = [];
+let palavraSecretaCategoria;
+let palavraSecretaSorteada;
+let palavras = [];
+let jogoAutomatico = true;
+
+carregaListaAutomatica();
+
+criarPalavraSecreta();
+function criarPalavraSecreta(){
+    const indexPalavra = parseInt(Math.random() * palavras.length)
+    
+    palavraSecretaSorteada = palavras[indexPalavra].nome;
+    palavraSecretaCategoria = palavras[indexPalavra].categoria;
+
+}
+
+montarPalavraNaTela();
+function montarPalavraNaTela(){
+    const categoria = document.getElementById("categoria");
+    categoria.innerHTML = palavraSecretaCategoria;
+
+    const palavraTela = document.getElementById("palavra-secreta");
+    palavraTela.innerHTML = "";
+    
+    for(i = 0; i < palavraSecretaSorteada.length; i++){  
+        if(listaDinamica[i] == undefined){
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
+            }
+            else{
+                listaDinamica[i] = "&nbsp;"
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }     
+        }
+        else{
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
+            }
+            else{
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }    
+        }
+    }   
+}
+
+function verificaLetraEscolhida(letra){
+    document.getElementById("tecla-" + letra).disabled = true;
+    if(tentativas > 0)
+    {
+        mudarStyleLetra("tecla-" + letra, false);
+        comparalistas(letra);
+        montarPalavraNaTela();
+    }    
+}
+
+function mudarStyleLetra(tecla, condicao){
+    if(condicao == false)
+    {
+        document.getElementById(tecla).style.background = "#C71585";
+        document.getElementById(tecla).style.color = "#ffffff";
+    }
+    else{
+        document.getElementById(tecla).style.background = "#008000";
+        document.getElementById(tecla).style.color = "#ffffff";
+    }
+
+    
+}
+
+function comparalistas(letra){
+    const pos = palavraSecretaSorteada.indexOf(letra)
+    if(pos < 0){
+        tentativas--
+        carregaImagemForca();
+
+        if(tentativas == 0){
+            abreModal("OPS!", "Não foi dessa vez ... A palavra secreta era <br>" + palavraSecretaSorteada);
+            piscarBotaoJogarNovamente(true);
+        }
+    }
+    else{
+        mudarStyleLetra("tecla-" + letra, true);
+        for(i = 0; i < palavraSecretaSorteada.length; i++){
+            if(palavraSecretaSorteada[i] == letra){
+                listaDinamica[i] = letra;
+            }
+        }
+    }
+    
+    let vitoria = true;
+    for(i = 0; i < palavraSecretaSorteada.length; i++){
+        if(palavraSecretaSorteada[i] != listaDinamica[i]){
+            vitoria = false;
+        }
+    }
+
+    if(vitoria == true)
+    {
+        abreModal("PARABÉNS!", "Você venceu...");
+        tentativas = 0;
+        piscarBotaoJogarNovamente(true);
+    }
+}
+
+
+async function atraso(tempo){
+    return new Promise(x => setTimeout(x, tempo))     
+}
+
+function carregaImagemForca(){
+    switch(tentativas){
+        case 5:
+            document.getElementById("imagem").style.background  = "url('./img/forca01.png')";
+            break;
+        case 4:
+            document.getElementById("imagem").style.background  = "url('./img/forca02.png')";
+            break;
+        case 3:
+            document.getElementById("imagem").style.background  = "url('./img/forca03.png')";
+            break;
+        case 2:
+            document.getElementById("imagem").style.background  = "url('./img/forca04.png')";
+            break;
+        case 1:
+            document.getElementById("imagem").style.background  = "url('./img/forca05.png')";
+            break;
+        case 0:
+            document.getElementById("imagem").style.background  = "url('./img/forca06.png')";
+            break;
+        default:
+            document.getElementById("imagem").style.background  = "url('./img/forca.png')";
+            break;
+    }
+}
+
+function abreModal(titulo, mensagem){
+    let modalTitulo = document.getElementById("exampleModalLabel");
+    modalTitulo.innerText = titulo;
+
+    let modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = mensagem;
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+let bntReiniciar = document.querySelector("#btnReiniciar")
+bntReiniciar.addEventListener("click", function(){
+    jogarNovamente = false;
+    location.reload();
+});
+
+function listaAutomatica(){ // ativa o modo manual
+    if (jogoAutomatico == true) {
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-play-circle'></i>"
+        palavras = [];
+        jogoAutomatico = false;
+
+        document.getElementById("abreModalAddPalavra").style.display = "block";
+        document.getElementById("status").innerHTML = "Modo Manual";
+    }
+    else if(jogoAutomatico == false){ // ativa o modo automático
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-pause-circle'></i>"
+        jogoAutomatico = true;
+
+        document.getElementById("abreModalAddPalavra").style.display = "none";
+        document.getElementById("status").innerHTML = "Modo Automático";
+        
+    }
+}
+
+const modal = document.getElementById("modal-alerta");
+
+const btnAbreModal = document.getElementById("abreModalAddPalavra");
+btnAbreModal.onclick = function(){
+    modal.style.display = "block";
+}
+
+const btnFechaModal = document.getElementById("fechaModal");
+btnFechaModal.onclick = function(){ 
+    modal.style.display = "none";
+    document.getElementById("addPalavra").value = "";
+    document.getElementById("addCategoria").value = ""; 
+}
+
+window.onclick = function(){ 
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("addPalavra").value = "";
+        document.getElementById("addCategoria").value = ""; 
+    }  
+}
+
+function carregaListaAutomatica(){
+    palavras = [
+        palavra001 = {
+            nome: "PYTHON",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra002 = {
+            nome: "JAVA",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra003 = {
+            nome: "PHP",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra004 = {
+            nome: "REACT",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra005 = {
+            nome: "TYPESCRIPT",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra006 = {
+            nome: "JAVASCRIPT",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra007 = {
+            nome: "ASSEMBLY",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra008 = {
+            nome: "SWIFT",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra009 = {
+            nome: "RUBY",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra010 = {
+            nome: "LINGUAGEM C",
+            categoria:"LINGUAGEM DE PROGRAMAÇÃO"
+        },
+        palavra011 = {
+            nome: "THE FOREST",
+            categoria:"JOGOS"
+        },
+        palavra012 = {
+            nome: "FORZA HORIZON",
+            categoria:"JOGOS"
+        },
+        palavra013 = {
+            nome: "VALORANT",
+            categoria:"JOGOS"
+        },
+        palavra014 = {
+            nome: "RESIDENT EVIL",
+            categoria:"JOGOS"
+        },
+        palavra015 = {
+            nome: "LEAGUE OF LEGENDS",
+            categoria:"JOGOS"
+        },
+        palavra016 = {
+            nome: "FORTNITE",
+            categoria:"JOGOS"
+        },
+        palavra017 = {
+            nome: "MINECRAFT",
+            categoria:"JOGOS"
+        },
+        palavra018 = {
+            nome: "THE WITCHER",
+            categoria:"JOGOS"
+        },
+        palavra019 = {
+            nome: "ELDEN RING",
+            categoria:"JOGOS"
+        },
+        palavra020 = {
+            nome: "GOD OF WAR",
+            categoria:"JOGOS"
+        },
+        palavra021 = {
+            nome: "A ERA DO GELO",
+            categoria:"TV E CINEMA"
+        },
+        palavra022 = {
+            nome: "HOMEM ARANHA",
+            categoria:"TV E CINEMA"
+        },
+        palavra023 = {
+            nome: "CASA MONSTRO",
+            categoria:"TV E CINEMA"
+        },
+        palavra024 = {
+            nome: "NARUTO",
+            categoria:"TV E CINEMA"
+        },
+        palavra025 = {
+            nome: "STRANGER THINGS",
+            categoria:"TV E CINEMA"
+        },
+        palavra026 = {
+            nome: "O HOBBIT",
+            categoria:"TV E CINEMA"
+        },
+        palavra027 = {
+            nome: "MULHER MARAVILHA",
+            categoria:"TV E CINEMA"
+        },
+        palavra028 = {
+            nome: "O INCRIVEL HULK",
+            categoria:"TV E CINEMA"
+        },
+        palavra029 = {
+            nome: "BOB ESPONJA",
+            categoria:"TV E CINEMA"
+        },
+        palavra030 = {
+            nome: "HARRY POTTER",
+            categoria:"TV E CINEMA"
+        }
+    ];
+}
+
+function adicionarPalavra(){
+    let addPalavra = document.getElementById("addPalavra").value.toUpperCase();
+    let addCategoria = document.getElementById("addCategoria").value.toUpperCase();
+
+    if (isNullOrWhiteSpace(addPalavra) || isNullOrWhiteSpace(addCategoria) || addPalavra.length < 3 || addCategoria.length < 3) {
+        abreModal("ATENÇÃO"," Palavra e/ou Categoria inválidos");
+        return;
+    }
+
+    let palavra = {
+        nome: addPalavra,
+        categoria: addCategoria
+    }
+
+    palavras.push(palavra);  
+    sortear();
+    
+    document.getElementById("addPalavra").value = "";
+    document.getElementById("addCategoria").value = "";
+}
+
+function isNullOrWhiteSpace(input){
+    return !input || !input.trim();
+}
+
+function sortear(){
+    if(jogoAutomatico == true){
+        location.reload();  
+    }
+    else{
+        if(palavras.length > 0){
+            listaDinamica=[];
+            criarPalavraSecreta();
+            montarPalavraNaTela();
+            resetaTeclas();
+            tentativas = 6;
+            piscarBotaoJogarNovamente(false);
+        }
+    }
+}
+
+function resetaTeclas(){
+    let teclas = document.querySelectorAll(".teclas > button")
+    teclas.forEach((x) =>{
+        x.style.background = "#FFFFFF";
+        x.style.color = "#8B008B";
+        x.disabled = false;
+    });
+}
+
+
+async function piscarBotaoJogarNovamente(querJogar){
+    if(querJogar){
+        document.getElementById("jogarNovamente").style.display = "block";
+    }
+    else{
+        document.getElementById("jogarNovamente").style.display = "none";
+    }
+}
+
+
